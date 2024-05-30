9aac5bb9abfacc2764d37ba5c827c7396e9351c8
NoA <104156659+NoAys@users.noreply.github.com>
2024-05-30 18:17:49 +0900

    Create main.java

---
+++ 24.05.30/ex 2884 : 알람 시계/main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if (M >= 45) {
+			M -= 45;
+			System.out.println(H+" "+M);
+		} else if (M < 45 && H > 0) {
+			H--;
+			M += 15;
+			System.out.println(H+" "+M);
+		} else if (M < 45 && H == 0) {
+			H += 23;
+			M += 15;
+			System.out.println(H+" "+M);
+		}
+		
+	}
+
+}

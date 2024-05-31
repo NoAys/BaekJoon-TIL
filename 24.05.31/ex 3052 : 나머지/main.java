ae2221195085486b6c5819b5e92ae954cf26d1e5
NoA <104156659+NoAys@users.noreply.github.com>
2024-05-31 22:28:44 +0900

    Create main.java

---
+++ 24.05.31/ex 3052 : 나머지/main.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> a = new HashSet<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			a.add(sc.nextInt() % 42);
+		}
+		
+		System.out.println(a.size());
+	}
+
+}

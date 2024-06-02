084b3e4736cb3a9ad955b2590707986f21fab820
NoA <104156659+NoAys@users.noreply.github.com>
2024-06-02 23:34:49 +0900

    Update main.java

--- 24.06.02/ex 1546 : 평균/main.java
+++ 24.06.02/ex 1546 : 평균/main.java
@@ -1,1 +1,31 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		double score[] = new double[a]; 
+		double avg, max = 0,sum = 0;
+		
+		for(int i=0;i<score.length;i++) {
+			score[i] = sc.nextInt(); 
+			
+			if(score[i]>max) 
+				max = score[i]; 
+		}
+		
+		for(int i=0;i<score.length;i++) {
+			score[i] = (score[i]/max)*100;
+			sum += score[i];
+		}
+		sc.close();
+		
+		avg = sum / a;
+		
+		System.out.println(avg);
+	}
+}

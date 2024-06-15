0f57ed3baee70f2f8f9a13d76b7dd0d4414264a3
NoA <104156659+NoAys@users.noreply.github.com>
2024-06-15 17:32:28 +0900

    Create main.java

---
+++ 24.06.15/ex 1110 : 더하기 사이클/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int first = N;
+		int count = 0;
+		
+		while (true) {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			count++;
+			
+			if (first == N) {
+				break;
+			}
+		}
+		System.out.println(count);
+	}
+}

6725ada99ea0e48c322081a2274b054b65c9a30d
NoA <104156659+NoAys@users.noreply.github.com>
2024-06-28 17:27:37 +0900

    Create main.java

---
+++ 24.06.28/ex 10811 : 바구니 뒤집기/main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+		
+		for (int i = 0; i < M; i++) {
+			int a = sc.nextInt() - 1;
+			int b = sc.nextInt() - 1;
+			
+			while (a < b) {
+				int temp = arr[a];
+				arr[a] = arr[b];
+				arr[b] = temp;
+				a++;
+				b--;
+			}
+		}
+		
+		for (int i : arr) {
+			System.out.printf("%d ", i);
+		}
+	}
+
+}
